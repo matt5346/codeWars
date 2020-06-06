9b343aac6c60ff262d2c23224fb1137278f07012
matt 2019 <matvey20.18@bk.ru>
2020-06-06 23:42:45 +0300

    update

--- 8kataAbbreviate/index.js
+++
@@ -1,6 +0,0 @@
-function abbrevName(name){
-    let array_name = name.split(' ');
-    return array_name[0].charAt(0).toUppercase() + '.' + array_name[1].charAt(0).toUppercase();
-}
-
-console.log(abbrevName("Sam Harris"));

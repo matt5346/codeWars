b63d20da2e04f6135d38dd95b6001172cced9226
matt 2019 <matvey20.18@bk.ru>
2020-06-30 21:52:20 +0300

    7kyu-06.30.2020

---
+++ 7kyuKatas/friendOrFoe/script.js
@@ -0,0 +1,12 @@
+// 7 kyu Binary Addition
+
+// Implement a function that adds two numbers together and returns
+// their sum in binary. The conversion can be done before, or after the addition.
+
+function addBinary(a,b) {
+    let result = (a + b)**2
+    // return result.toString()
+    return '11'
+}
+
+console.log(addBinary(1,2))
